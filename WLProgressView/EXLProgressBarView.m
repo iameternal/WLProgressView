0f3e38950f26dacc2a9114a95e0529dbf1e307cd
liuyabo <liuyabo_weixiao@163.com>
2017-07-04 17:21:34 +0800

    添加progress view

--- WLProgressView/EXLProgressBarView.m
+++ WLProgressView/EXLProgressBarView.m
@@ -9,6 +9,14 @@
 #import "EXLProgressBarView.h"
 
 @implementation EXLProgressBarView
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        [self addSubview:self.progressTintView];
+        [self.progressTintView addSubview:self.trackTintView];
+    }
+    return self;
+}
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
@@ -37,14 +45,14 @@
 - (UIView *)progressTintView {
     if (!_progressTintView) {
         _progressTintView = [UIView new];
-        _progressTintView.backgroundColor = EXLColor(240, 240, 240);
+        _progressTintView.backgroundColor = [UIColor colorWithRed:240/255.0 green:240/255.0 blue:240/255.0 alpha:1];
     }
     return _progressTintView;
 }
 - (UIView *)trackTintView {
     if (!_trackTintView) {
         _trackTintView = [UIView new];
-        _trackTintView.backgroundColor = EXLColor(165, 207, 255);
+        _trackTintView.backgroundColor = [UIColor colorWithRed:165/255.0 green:207/255.0 blue:255/255.0 alpha:1];
     }
     return _trackTintView;
 }
@@ -56,7 +64,9 @@
 }
 - (void)setProgress:(float)progress {
     _progress = progress;
-    self.trackTintView.width = self.progressTintView.width * progress;
+    CGRect frame = self.trackTintView.frame;
+    frame.size.width = self.progressTintView.frame.size.width * progress;
+    self.trackTintView.frame = frame;
 }
 - (void)displayWithInterval:(float)interval numberOfCopies:(NSInteger)copies animated:(BOOL)animated {
     if (animated) {
@@ -66,25 +76,34 @@
             ++ count;
             if (weakSelf.progress == 0) {
                 [timer invalidate];
-                weakSelf.trackTintView.width = 0;
+                CGRect frame = weakSelf.trackTintView.frame;
+                frame.size.width = 0;
+                weakSelf.trackTintView.frame = frame;
             } else {
                 if ((float)count/copies >= weakSelf.progress) {
                     [timer invalidate];
-                    weakSelf.trackTintView.width = weakSelf.progressTintView.width * weakSelf.progress;
+                    CGRect frame = weakSelf.trackTintView.frame;
+                    frame.size.width = weakSelf.progressTintView.frame.size.width * weakSelf.progress;
+                    weakSelf.trackTintView.frame = frame;
                 } else {
-                    weakSelf.trackTintView.width = weakSelf.progressTintView.width * ((float)count/copies);
+                    CGRect frame = weakSelf.trackTintView.frame;
+                    frame.size.width = weakSelf.progressTintView.frame.size.width * ((float)count/copies);
+                    weakSelf.trackTintView.frame = frame;
                 }
             }
         }];
     } else {
         if (self.progress == 0) {
-            self.trackTintView.width = 0;
+            CGRect frame = self.trackTintView.frame;
+            frame.size.width = 0;
+            self.trackTintView.frame = frame;
         } else {
-            self.trackTintView.width = self.progressTintView.width * self.progress;
+            CGRect frame = self.trackTintView.frame;
+            frame.size.width = self.progressTintView.frame.size.width * self.progress;
+            self.trackTintView.frame = frame;
         }
     }
 }
-
 /*
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
--- WLProgressView/ViewController.m
+++ WLProgressView/ViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "ViewController.h"
+#import "EXLProgressBarView.h"
 
 @interface ViewController ()
+@property (strong, nonatomic) EXLProgressBarView *progressView;
 
 @end
 
@@ -17,13 +19,32 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:self.progressView];
 }
-
+- (void)viewDidLayoutSubviews{
+    [super viewDidLayoutSubviews];
+    __weak typeof(self) weakSelf = self;
+    [self.progressView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerY.mas_equalTo(weakSelf.view.mas_centerY).offset(0);
+        make.left.mas_equalTo(weakSelf.view.mas_left).offset(20);
+        make.right.mas_equalTo(weakSelf.view.mas_right).offset(-20);
+        make.height.mas_equalTo(20);
+    }];
+    self.progressView.progress = 0.9;
+    self.progressView.cornerRadius = 12.0;
+    [self.progressView displayWithInterval:0.01 numberOfCopies:1000 animated:YES];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-
+- (EXLProgressBarView *)progressView {
+    if (!_progressView) {
+        _progressView = [[EXLProgressBarView alloc] init];
+    }
+    return _progressView;
+}
 @end
